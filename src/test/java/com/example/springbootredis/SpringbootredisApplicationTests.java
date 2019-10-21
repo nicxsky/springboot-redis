1882cb73e33c95fb825ecffb16780adc0bcf3e2e
yudaoqing <yudq@greatld.com>
2019-10-21 15:08:00 +0800

    add RedisUtil

--- src/test/java/com/example/springbootredis/SpringbootredisApplicationTests.java
+++ src/test/java/com/example/springbootredis/SpringbootredisApplicationTests.java
@@ -1,13 +1,34 @@
 package com.example.springbootredis;
 
+import com.example.springbootredis.util.RedisUtil;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootredisApplicationTests {
 
+    @Autowired
+    private RedisUtil redisUtil;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testRedis(){
+        System.out.println("key is exist:" + redisUtil.hasKey("key_test"));
+
+        long expire = redisUtil.getExpire("key_test");
+        System.out.println(expire);
+
+        System.out.println("set expire:" + redisUtil.setExpire("key_test", 10));
+
+        expire = redisUtil.getExpire("key_test");
+        System.out.println(expire);
+
+        System.out.println("key is exist:" + redisUtil.hasKey("key_test"));
+
     }
 
 }
